36cfbb4cf9c5139b0d42ddeec37bb30256b5492f
itpermatagbkp <itpermatagbkp@gmail.com>
2024-07-01 00:12:26 +0700

    Create api for POST add data categories

---
+++ config/routes.php
@@ -0,0 +1,43 @@
+<?php
+
+use Cake\Routing\Route\DashedRoute;
+use Cake\Routing\RouteBuilder;
+
+return function (RouteBuilder $routes): void {
+    $routes->setRouteClass(DashedRoute::class);
+    $routes->scope('/api', function (RouteBuilder $builder): void {
+        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+        $builder->connect('/categories/add', ['controller' => 'Categories', 'action' => 'add', '_method' => 'POST']);
+        $builder->connect('/pages/*', 'Pages::display');
+        $builder->fallbacks();
+    });   
+
+    /*
+     * If you need a different set of middleware or none at all,
+     * open new scope and define routes there.
+     *
+     * ```
+     * $routes->scope('/api', function (RouteBuilder $builder): void {
+     *     // No $builder->applyMiddleware() here.
+     *
+     *     // Parse specified extensions from URLs
+     *     // $builder->setExtensions(['json', 'xml']);
+     *
+     *     // Connect API actions here.
+     * });
+     * ```
+     */
+};
+
+// return function(RouteBuilder $routes){
+//     $routes->setRouteClass(DashedRoute::class);
+//     $routes->scope('/api', function(RouteBuilder $builder){
+//         $builder->connect('/categories/add', 
+//                             [
+//                             'controller' => 'Categories', 
+//                             'action' => 'add', 
+//                             '_method' => 'POST'
+//                         ]);
+//     });
+//     $routes->fallbacks(DashedRoute::class);
+// };
---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,127 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+use App\Controller\AppController;
+use Cake\Http\Middleware\CsrfProtectionMiddleware;
+
+class CategoriesController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+
+     public function initialize(): void
+     {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+        // $this->getEventManager()->off($this->Csrf);
+     }
+
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        
+        parent::beforeFilter($event);
+
+        // Nonaktifkan CSRF untuk metode add
+        if ($this->getRequest()->getParam('action') === 'add') {
+            $csrf = new CsrfProtectionMiddleware();
+            $csrf->skipCheckCallback(function ($request) {
+                return true; // Skip CSRF check for add action
+            });
+
+            $this->getEventManager()->off('Controller.startup', [$csrf, 'checkCsrf']);
+            $this->getRequest()->getAttribute('csrf');
+        }
+    }
+
+    public function index()
+    {
+        $categories = $this->paginate($this->Categories);
+        $this->set(compact('categories'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Category id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $category = $this->Categories->get($id, [
+            'contain' => [],
+        ]);
+
+        $this->set(compact('category'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $category = $this->Categories->newEmptyEntity();
+        $category = $this->Categories->patchEntity($category, $this->request->getData());
+        if ($this->Categories->save($category)) {
+            $message = 'Saved';
+        } else {
+            $message = 'Error';
+        }
+        $this->set([
+            'message' => $message,
+            'category' => $category,
+            '_serialize' => ['message', 'category']
+        ]);
+    }
+    
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Category id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $category = $this->Categories->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $category = $this->Categories->patchEntity($category, $this->request->getData());
+            if ($this->Categories->save($category)) {
+                $this->Flash->success(__('The category has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The category could not be saved. Please, try again.'));
+        }
+        $this->set(compact('category'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Category id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $category = $this->Categories->get($id);
+        if ($this->Categories->delete($category)) {
+            $this->Flash->success(__('The category has been deleted.'));
+        } else {
+            $this->Flash->error(__('The category could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
